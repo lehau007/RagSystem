ea042e46c3a0d7e0528ba39eb74b6d62a8d0c58b
lehau007 <lehaulm007@gmail.com>
2025-10-22 16:43:11 +0700

    Nearly completed version

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # Import ST
-
+from chatbot import Chatbot
+from dotenv import load_dotenv
 if __name__ == "__main__":
-    pass 
+    load_dotenv()
+    hf_token = load_dotenv()
+    ossapi_key = load_dotenv()
+    chatbot = Chatbot(ossapi_key, hf_token)
+
+    while True:
+        user_input = input("Enter a prompt and # if you want to end: ")
+        if user_input.endswith("#"):
+            break
+
+        response = chatbot.chat(user_input)
+        print("Chatbot response: ", response["response"])
---
+++ readme.md
@@ -0,0 +1,6 @@
+# RagSystem
+
+In this project, i define a Rag workflow from processing document, to retrieve context. Model can decide to use tool or not
+
+demo_img 
+path = "DemoImg/conversation"
